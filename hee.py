4d34055509249cb2894775c9906711752d16882f
ishanhe97 <ishanhe97@gmail.com>
2022-09-24 22:11:22 +0530

    2nd comi

--- hee.py
+++ hee.py
@@ -1,1 +1,10 @@
 print("hekko gucker")
+import time
+import sys
+from datetime import datetime
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUi
+import gi  #it is a binding. named pyGobject
+gi.require_version("Gst", "1.0")
